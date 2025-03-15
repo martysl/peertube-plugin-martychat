aac5f8bacc991181e6ea3333ed15786ec085d292
Marty AI <78570936+martysl@users.noreply.github.com>
2025-03-16 00:43:34 +0100

    Create MessageList.tsx

---
+++ src/components/MessageList.tsx
@@ -0,0 +1,16 @@
+interface MessageProps {
+  messages: Message[];
+}
+
+export default function MessageList({ messages }: MessageProps) {
+  return (
+    <div className="flex-grow overflow-y-auto p-2 h-64 border border-gray-700 rounded-md">
+      {messages.map((msg, index) => (
+        <div key={index} className="p-1">
+          <b>{msg.name}:</b> {msg.message}
+          {msg.image && <img src={msg.image} alt="Uploaded" className="max-w-xs mt-2" />}
+        </div>
+      ))}
+    </div>
+  );
+}
