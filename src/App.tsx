ef2c86e6491fa25304e54e023a7b2cac86dd424d
Marty AI <78570936+martysl@users.noreply.github.com>
2025-03-16 02:27:02 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import ChatWindow from "./components/ChatWindow";
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <ChatWindow />
+    </div>
+  );
+}
